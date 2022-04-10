d6c600ee47a6db69e68ded27756a5da30fa6a351
Faiyaz Rahman <faiyazrahman03@gmail.com>
2022-04-11 00:28:05 +0600

    user get and create

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,14 +1,24 @@
 import { NextFunction, Request, Response } from 'express';
+import User from '../models/User';
 import catchAsync from '../utils/catchAsync';
 
 //fetch all user
-export const getUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
+export const getUsers = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+   const users = await User.find({});
+   res.status(200).json({ status: 'success', message: 'Users fetched successfully.', users });
+});
 
 //fetch user by id
-export const getUserById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
+export const getUserById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+   const user = await User.findById(req.params.id);
+   res.json({ status: 'success', message: 'User fetched successfully', user });
+});
 
 //create or register user
-export const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
+export const createUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+   const user = await User.create({ ...req.body });
+   res.status(200).json({ status: 'success', message: 'User created successflly', user });
+});
 
 //edit or update user
 export const updateUser = catchAsync(async (req: Request, res: Response, next: NextFunction) => {});
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,7 @@
 //dependencies
+import { hash } from 'bcrypt';
 import { model, Schema } from 'mongoose';
-
+import AppError from '../utils/AppError';
 //define user interface
 interface User {
    name: string;
@@ -12,9 +13,19 @@ interface User {
 //create schema
 const userSchema: Schema = new Schema<User>({
    name: { required: true, type: String, trim: true },
-   email: { required: true, type: String, trim: true, lowercase: true },
-   phone: { required: true, type: String, trim: true },
+   email: { required: true, unique: true, type: String, trim: true, lowercase: true },
+   phone: { required: true, unique: true, type: String, trim: true },
    password: { required: true, type: String, trim: true },
+});
+
+userSchema.pre('save', async function save(next) {
+   if (!this.isModified('password')) return next();
+   try {
+      this.password = await hash(this.password, 12);
+      return next();
+   } catch (error) {
+      return next(new AppError(500, 'Error while hasing the password'));
+   }
 });
 
 //create mongoose
